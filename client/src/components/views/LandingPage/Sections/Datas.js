e96bd630c38ab266bc7d7ceda73da489b8868dc0
OktriSaputra <oktriwaw@gmail.com>
2020-05-16 21:29:40 +0700

    Update
    Update

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,72 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "Rp.10.000 to Rp.30.000",
+        "array": [10000, 30000]
+    },
+    {
+        "_id": 2,
+        "name": "Rp.40.000 to Rp.60.000",
+        "array": [40000, 30000]
+    },
+    {
+        "_id": 3,
+        "name": "Rp.70.000 to Rp.80000",
+        "array": [70000, 80000]
+    },
+    {
+        "_id": 4,
+        "name": "Rp.90.000 to Rp.100.000",
+        "array": [90000, 100000]
+    },
+    {
+        "_id": 5,
+        "name": "More than Rp.100.000",
+        "array": [100000, 1500000]
+    }
+]
+
+
+
+const continents = [
+    {
+        "_id": 1,
+        "name": "Tepung"
+    },
+    {
+        "_id": 2,
+        "name": "Keju"
+    },
+    {
+        "_id": 3,
+        "name": "Butter"
+    },
+    {
+        "_id": 4,
+        "name": "Margarin"
+    },
+    {
+        "_id": 5,
+        "name": "Glaze"
+    },
+    {
+        "_id": 6,
+        "name": "Pewarna Makanan"
+    },
+    {
+        "_id": 7,
+        "name": "Susu"
+    }
+]
+
+
+export {
+    price,
+    continents
+}
--- mystore-back-end/database/connection.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    connection: "mongodb+srv://<username>:<password>@cluster0-vyg6a.mongodb.net/test?retryWrites=true&w=majority"
-}
