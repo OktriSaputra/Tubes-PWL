71e454b63c8a7ecf1a2a76c31c5fa834ec111250
Sartiah <tyalampung2017@gmail.com>
2020-05-17 01:03:15 +0700

    Update

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -5,13 +5,14 @@ const MenuItemGroup = Menu.ItemGroup;
 
 function LeftMenu(props) {
   return (
-    <Menu mode={props.mode}>
-    <Menu.Item key="mail">
-      <a href="/"><h2>Home</h2></a>
-    </Menu.Item>
-    <SubMenu title={<span><h2>Contact</h2></span>}>
-    </SubMenu>
-  </Menu>
+  <Menu mode={props.mode}>
+        <Menu.Item key="mail">
+          <a href="/"><h2>Home</h2></a>
+        </Menu.Item>
+        <Menu.Item key="app">
+          <a href="product"><h2>Product</h2></a>
+        </Menu.Item>
+      </Menu>
   )
 }
 
